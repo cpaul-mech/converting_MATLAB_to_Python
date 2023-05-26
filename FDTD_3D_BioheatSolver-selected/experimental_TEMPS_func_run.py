2a1f3505c48b38a67bb144b6c4d918f34f8772db
cpaul-mech <cpaul.mech@gmail.com>
2023-05-26 13:37:08 -0600

    nothin important

--- FDTD_3D_BioheatSolver-selected/experimental_TEMPS_func_run.py
+++ FDTD_3D_BioheatSolver-selected/experimental_TEMPS_func_run.py
@@ -29,7 +29,7 @@ nFZ = matlab_data["nFZ"]
 
 
 (Temps, function_time) = calc_TEMPS_v045(Modl,t0,Vox, dt, ht,ct,rho,k,cp,wType,w,Q_s,nFZ,tacq, Tb, Bc)
-sio.savemat('py_vars_to_check.mat', {'time': function_time, 'TEMPS':Temps})
-plt.plot(function_time, np.squeeze(Temps[70,70,59,:]))
-plt.show()
+# sio.savemat('py_vars_to_check.mat', {'time': function_time, 'TEMPS':Temps})
+# plt.plot(function_time, np.squeeze(Temps[70,70,59,:]))
+# plt.show()
 
